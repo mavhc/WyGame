2b430f6a3ab12092f9d5848c03931f2419360a9c
Richard-CRT <Apples282@hotmail.co.uk>
2015-12-30 12:45:18 +0000

    Switched Crosses to images

---
+++ web/script.js
@@ -0,0 +1,8 @@
+﻿function pageLoad() {
+    var divs = document.getElementsByClassName('cell');
+    [].slice.call(divs).forEach(function (div) {
+        div.innerHTML = '<img src="images/cross.png" height="100%" width="100%" align="top" />';
+    });
+}
+
+window.onload = pageLoad();
