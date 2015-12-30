72c7ecef931d3a6b3001dbab90c6c2a3aa78ef25
Richard-CRT <Apples282@hotmail.co.uk>
2015-12-30 15:06:18 +0000

    Can hide and show crosses by clicking them

--- web/script.js
+++ web/script.js
@@ -1,7 +1,22 @@
 ﻿function pageLoad() {
     var divs = document.getElementsByClassName('cell');
     [].slice.call(divs).forEach(function (div) {
-        div.innerHTML = '<img src="images/cross.png" height="100%" width="100%" align="top" />';
+        div.innerHTML = '<img class="crossImg" src="images/cross.png" height="100%" width="100%" align="top" />';
+    });
+}
+
+function cellClicked(div) {
+    var children = div.childNodes;
+    [].slice.call(children).forEach(function (child) {
+        if (child.className == "crossImg")
+        {
+            if (child.style.visibility == "hidden") {
+                child.style.visibility = 'visible';
+            }
+            else {
+                child.style.visibility = 'hidden';
+            }
+        }
     });
 }
 
